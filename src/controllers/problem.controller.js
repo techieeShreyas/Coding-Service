7ead26ccba46e643120b8d8718da0c0565ab8015
techieShreyas <shreyasagrawaltechie@gmail.com>
2024-10-29 15:24:39 +0530

    Feat: Added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,31 +1,35 @@
-function pingProblemController(req, res)
-{
-    return res.json({message: "Problem controller is up"});
+function pingProblemController(req, res) {
+    return res.json({ message: "Problem controller is up" });
 }
 
-function addProblem(req, res)
-{
-
+function addProblem(req, res) {
+    return res.status(501).json({
+        message: "Not implemented"
+    });
 }
 
-function getProblem(req, res)
-{
-
+function getProblem(req, res) {
+    return res.status(501).json({
+        message: "Not implemented"
+    });
 }
 
-function getProblems(req, res)
-{
-
+function getProblems(req, res) {
+    return res.status(501).json({
+        message: "Not implemented"
+    });
 }
 
-function deleteProblem(req, res)
-{
-
+function deleteProblem(req, res) {
+    return res.status(501).json({
+        message: "Not implemented"
+    });
 }
 
-function updateProblem(req, res)
-{
-
+function updateProblem(req, res) {
+    return res.status(501).json({
+        message: "Not implemented"
+    });
 }
 
 module.exports = {
